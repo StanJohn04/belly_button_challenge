e1d47def59f9067a6246d7d45b385cdeee3e392e
StanJohn04 <stanjohn04@gmail.com>
2023-05-24 13:25:59 -0600

    updated gauge in bonus.js

--- docs/static/js/bonus.js
+++ docs/static/js/bonus.js
@@ -1,5 +1,3 @@
-
-
 function buildGauge(id){
 
     d3.json(url).then(function(data){
@@ -13,15 +11,15 @@ function buildGauge(id){
         let wfreq = parseInt(selectSample.wfreq)
 
         let degree = wfreq * (180/10);
-        console.log(wfreq)
+
         // Trig to calc meter point
-        let degrees = 180 - degree;
-        let radius = .5;
+        let degrees = 175 - degree;
+        let radius = .75;
         let radians = degrees * Math.PI / 180;
         let x = radius * Math.cos(radians);
         let y = radius * Math.sin(radians);
 
-        let mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+        let mainPath = 'M -.0 -0.025 L .0 0.05 L ',
             pathX = String(x),
             space = ' ',
             pathY = String(y),
@@ -35,20 +33,61 @@ function buildGauge(id){
             name: 'WASH FREQ',
             text: wfreq,
             hoverinfo: 'text+name'},
-        { values: [1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
-        rotation: 90,
-        text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1',''],
-        textinfo: 'text',
-        textposition:'inside',
-        textfont:{
-            size : 16,
+        { domain: { x: [0, 1], y: [0, 1] },
+		value: 0,
+		title: { text: "Belly Button Washing Frequency" },
+		type: "indicator",
+		mode: "gauge",
+        gauge:{
+            showticklabels:true,
+            axis:{
+                range: [0,9],
+                visible: true,
+                showticklabels:true,
+                ticks: "outside",
+                tickvals: [0,1,2,3,4,5,6,7,8,9],
+                ticktext:["0","1","2","3","4","5","6","7","8",'9']
             },
-        marker: {colors:['rgb(23, 119, 23)','rgb(89, 192, 89)','rgb(126, 194, 126)','rgb(155, 196, 155)','rgb(175, 210, 175)','rgb(225, 235, 206)','rgb(238, 238, 226)','rgb(238, 238, 204)','rgb(235, 235, 178)', 'white']},
-        labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '2-1', '0-1',''],
-        hoverinfo: 'text',
-        hole: .5,
-        type: 'pie',
-        showlegend: false
+            steps:[
+                {
+                    range:[0,1],
+                    color:'rgb(235, 235, 178)',
+                },
+                {
+                    range:[1,2],
+                    color:'rgb(238, 238, 204)'
+                },
+                {
+                    range:[2,3],
+                    color:'rgb(238, 238, 226)'
+                },
+                {
+                    range:[3,4],
+                    color:'rgb(225, 235, 206)'
+                },
+                {
+                    range:[4,5],
+                    color:'rgb(175, 210, 175)'
+                },
+                {
+                    range:[5,6],
+                    color:'rgb(155, 196, 155)'
+                },
+                {
+                    range:[6,7],
+                    color:'rgb(126, 194, 126)'
+                },
+                {
+                    range:[7,8],
+                    color:'rgb(89, 192, 89)'
+                },
+                {
+                    range:[8,9],
+                    color:'rgb(23, 119, 23)'
+                }
+
+            ]
+        }
         }];
 
         let layout = {
@@ -60,12 +99,10 @@ function buildGauge(id){
                 color: '#2F6497'
             }
             }],
-
-        title: '<b>Belly Button Washing Frequency</b> <br> <b>Scrub Per Week</b>',
         xaxis: {zeroline:false, showticklabels:false,
                     showgrid: false, range: [-1, 1]},
         yaxis: {zeroline:false, showticklabels:false,
-                    showgrid: false, range: [-1, 1]},
+                    showgrid: false, range: [-1, 2]},
         width: 600, height: 500, margin: { t: 0, b: 0 }, padding:0 
         };
 
